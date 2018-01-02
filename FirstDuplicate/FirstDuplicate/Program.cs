939e487d1ba326a253fe6eaae4cebe6de86f7dc5
Neous1 <Neous1@users.noreply.github.com>
2018-01-01 23:31:35 -0500

    push to laptop

---
+++ FirstDuplicate/FirstDuplicate/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            int [] a = { 2,3,3,1,5,2};
+
+
+            /*
+            iterate array
+            print a[0]
+            set a[a[0]] to -a[a[0]]
+            */
+
+//            for (int i = 0; i < a.Length; i++)
+//            {
+//                if (a[Math.Abs(a[i])] > 0 )
+//                {
+//                    a[Math.Abs(a[i])] = -a[Math.Abs(a[i])];
+//                    Console.WriteLine(a[Math.Abs(a[i])]);
+//                    Console.WriteLine(a[Math.Abs(a[i])]);
+                   
+//                    }
+//                break;
+////                //find first dup
+////
+////                else
+////                Console.WriteLine -1;
+/// 
+///        
+            
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("HI");
+            }
+            
+            
+
+            
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
