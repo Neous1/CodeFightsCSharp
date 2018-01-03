faba5c0b3db2231668a063ab28f1753eea70f7dc
Neous1 <Neous1@users.noreply.github.com>
2018-01-02 23:59:49 -0500

    more changes

--- FirstDuplicate/FirstDuplicate/Program.cs
+++ FirstDuplicate/FirstDuplicate/Program.cs
@@ -14,30 +14,6 @@ namespace FirstDuplicate
 
 
             int [] a = { 2,3,3,1,5,2};
-
-
-            /*
-            iterate array
-            print a[0]
-            set a[a[0]] to -a[a[0]]
-            */
-
-//            for (int i = 0; i < a.Length; i++)
-//            {
-//                if (a[Math.Abs(a[i])] > 0 )
-//                {
-//                    a[Math.Abs(a[i])] = -a[Math.Abs(a[i])];
-//                    Console.WriteLine(a[Math.Abs(a[i])]);
-//                    Console.WriteLine(a[Math.Abs(a[i])]);
-                   
-//                    }
-//                break;
-////                //find first dup
-////
-////                else
-////                Console.WriteLine -1;
-/// 
-///        
             
             var myDict = new Dictionary<int, int>();
 
@@ -49,11 +25,13 @@ namespace FirstDuplicate
 
             foreach (var item in myDict)
             {
-                if (myDict.Values)
-                {
-                    
-                }
                 Console.WriteLine(item);
+                if (myDict.ContainsValue(item.Value))
+                {
+                    Console.WriteLine(item.Value);
+                    break;
+                }
+                
             }
 
             Console.ReadLine();
