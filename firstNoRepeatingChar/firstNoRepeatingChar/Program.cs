ffde4e3e72119db6b4b3e83b35c068f948dc2e9d
Neous1 <Neous1@users.noreply.github.com>
2018-01-05 10:29:47 -0500

    create a dictionary

---
+++ firstNoRepeatingChar/firstNoRepeatingChar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstNoRepeatingChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abacabad";
+        }
+        
+        public static char firstNotRepeatingChar()
+        {
+            var char = new Dictionary<char, int>();
+
+
+        }
+    }
+}
